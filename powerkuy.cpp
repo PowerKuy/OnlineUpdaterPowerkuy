9167a046b48d012275e7c462513b96e6303957e8
PowerKuy <arieldany09@gmail.com>
2021-07-03 10:35:59 +0700

    update

--- powerkuy.cpp
+++ powerkuy.cpp
@@ -5,5 +5,5 @@
 "Info": "Fixed Some Bug",
 "Changelogs": "Fixed Bot Disconnect every 5 Minute\nFixed SeekBar Text\nRemoved Bot Ping Console\nLogin Bot Delay now is 20-40 Second",
 "Link": "https://www.mediafire.com/file/3sjavuyo0sxdqtr/%255BFixed%255D_Mod_Menu_Growtopia_3.65.apk/file",
-"Premium": true
+"Premium": false
 }
