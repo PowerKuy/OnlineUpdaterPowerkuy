b72d4279ec2894db7b8912752468ce20e46d527b
PowerKuy <arieldany09@gmail.com>
2021-07-03 09:20:30 +0700

    j

--- powerkuy.cpp
+++ powerkuy.cpp
@@ -2,8 +2,8 @@
 "Version": 1,
 "Creator": "PowerKuy",
 "Title": "Mod Menu Update !!",
-"Info": "Hello :) , Im PowerKuy",
-"Changelogs": "Fixed Crash",
+"Info": "Fixing Some Bug",
+"Changelogs": "Fixed Bot Disconnect every 5 Minute\nFixed SeekBar Text\nRemoved Bot Ping Console",
 "Link": "https://youtu.be/0pnauF9RspA",
 "Premium": false
 }
