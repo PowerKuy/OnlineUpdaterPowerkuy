cdc74708106e171ddeb9c20f848fee13417171d8
PowerKuy <arieldany09@gmail.com>
2021-07-31 11:42:06 +0700

    a

--- powerkuy.cpp
+++ powerkuy.cpp
@@ -1,7 +1,7 @@
 {
 "Version": 4,
 "Creator": "PowerKuy",
-"Title": "Hello My User...",
+"Title": "Hello Guys,",
 "Info": "Never Download Mod Menu From Other Channel\nBecause its Stealer\nNever Download From Channel That Review My Mod Menu , Never Download It\nJust Download Original In My YT",
 "Changelogs": "Click Link Below , Its Real Channel YT",
 "Link": "https://youtube.com/c/PowerKuy",
