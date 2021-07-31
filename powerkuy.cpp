c5e55ce119b03348087f61f75b0b92c6ab69dd11
PowerKuy <arieldany09@gmail.com>
2021-07-31 12:09:02 +0700

    update

--- powerkuy.cpp
+++ powerkuy.cpp
@@ -3,7 +3,7 @@
 "Creator": "PowerKuy",
 "Title": "Hello Guys,",
 "Info": "Never Download Mod Menu From Other Channel\nBecause its Stealer\nNever Download From Channel That Review My Mod Menu , Never Download It\nJust Download Original In My YT",
-"Changelogs": "Click Link Below , Its Real Channel YT",
+"Changelogs": "Btw Just Wait Im Update\nClick Link Below , Its Real Channel YT",
 "Link": "https://youtube.com/c/PowerKuy",
 "Premium": false
 }
