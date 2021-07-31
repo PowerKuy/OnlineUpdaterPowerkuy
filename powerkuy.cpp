db00d9b39684ccf461f1ab8a2411dfed4c20ae84
PowerKuy <arieldany09@gmail.com>
2021-07-31 11:41:11 +0700

    i

--- powerkuy.cpp
+++ powerkuy.cpp
@@ -1,9 +1,9 @@
 {
-"Version": 2,
+"Version": 4,
 "Creator": "PowerKuy",
-"Title": "Mod Menu Update !!",
-"Info": "Fixed Some Bug",
-"Changelogs": "Fixed Bot Disconnect every 5 Minute\nFixed SeekBar Text\nRemoved Bot Ping Console\nLogin Bot Delay now is 20-40 Second",
-"Link": "https://www.mediafire.com/file/3sjavuyo0sxdqtr/%255BFixed%255D_Mod_Menu_Growtopia_3.65.apk/file",
+"Title": "Hello My User...",
+"Info": "Never Download Mod Menu From Other Channel\nBecause its Stealer\nNever Download From Channel That Review My Mod Menu , Never Download It\nJust Download Original In My YT",
+"Changelogs": "Click Link Below , Its Real Channel YT",
+"Link": "https://youtube.com/c/PowerKuy",
 "Premium": false
 }
