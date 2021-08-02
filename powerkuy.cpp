9da66d0d29d7501d40ddc4ac00a4358fbb2f8c33
PowerKuy <arieldany09@gmail.com>
2021-08-03 00:03:24 +0700

    a

--- powerkuy.cpp
+++ powerkuy.cpp
@@ -3,7 +3,7 @@
 "Creator": "PowerKuy",
 "Title": "Warning",
 "Info": "NEVER JOIN IN VISUALWORLD , SOMETHING ITS WRONG.. ",
-"Changelogs": "I WILL FIX THIS...",
+"Changelogs": "I WILL FIX THIS...\nNew Text : Eee Moderator In There and Banning People...F Nuked World...",
 "Link": "https://youtube.com/c/PowerKuy",
 "Premium": false
 }
