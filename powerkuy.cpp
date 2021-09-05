8bea91e49bbde84d63e50fb5662e3a636530d851
PowerKuy <64466136+PowerKuy@users.noreply.github.com>
2021-09-05 10:25:09 +0700

    beta
    beta

--- powerkuy.cpp
+++ powerkuy.cpp
@@ -1,9 +1,9 @@
 {
 "Version": 5,
 "Creator": "PowerKuy",
-"Title": "UPDATE !!",
-"Info": "- FIXED MULTIBOT DC EVERY 5 MIN",
-"Changelogs": "↓↓ Link Here ↓↓",
-"Link": "https://www.mediafire.com/file/3kj1m8it9ufq9z9/%255B_Fix_Multibot_3.67_%255D_Growtopia_3.67.apk/file",
+"Title": "Welcome To Beta Test",
+"Info": "Launcher Feature\n- Support Growtopia Sound\n- Using Growtopia 32bit\n- Minim Size (10 MB) only",
+"Changelogs": "Official Youtube ↓↓",
+"Link": "https://youtube.com/c/PowerKuy",
 "Premium": false
 }
