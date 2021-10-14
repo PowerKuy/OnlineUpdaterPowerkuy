72873b4c82fd4ee0b73818c56541cc2ac89b1995
PowerKuy <64466136+PowerKuy@users.noreply.github.com>
2021-10-14 20:31:05 +0700

    Update powerkuy.cpp

--- powerkuy.cpp
+++ powerkuy.cpp
@@ -1,5 +1,5 @@
 {
-"Version": 4,
+"Version": 3,
 "Creator": "PowerKuy",
 "Title": "About Multibot Autoban",
 "Info": "I am still trying to fix that , dont use Multibot for now, Wait until i fixed that Autoban \n This happend because ubisoft update his security\n\nLink Isnt Ready Yet",
