c7e316f1e51fc81b96f7e1363033259fff5f5095
PowerKuy <64466136+PowerKuy@users.noreply.github.com>
2021-10-14 09:28:39 +0700

    ndjdj

--- powerkuy.cpp
+++ powerkuy.cpp
@@ -1,9 +1,9 @@
 {
 "Version": 3,
 "Creator": "PowerKuy",
-"Title": "Welcome To Beta Test",
-"Info": "Launcher Feature\n- Support Growtopia Sound\n- Using Growtopia 32bit\n- Minim Size (10 MB) only",
-"Changelogs": "Official Youtube ↓↓",
-"Link": "https://youtube.com/c/PowerKuy",
+"Title": "About Multibot Autoban",
+"Info": "I am still trying to fix that , dont use Multibot for now, Wait until i fixed that Autoban \n This happend because ubisoft update his security",
+"Changelogs": "",
+"Link": "",
 "Premium": false
 }
