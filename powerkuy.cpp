b1286ab54f228443baa248b7fd752b948b46813f
PowerKuy <64466136+PowerKuy@users.noreply.github.com>
2021-10-15 14:15:07 +0700

    Update powerkuy.cpp

--- powerkuy.cpp
+++ powerkuy.cpp
@@ -1,5 +1,5 @@
 {
-"Version": 5,
+"Version": 3,
 "Creator": "PowerKuy",
 "Title": "Multibot Fix",
 "Info": "To avoid AutoSuspend , please use bot level 7+\nThis Multibot Isnt Fully Fixed , there is still ban chance\n\nClick Below to Download",
