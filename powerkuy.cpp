929219cf53794fb2fb13f9868544184cfdf51612
PowerKuy <64466136+PowerKuy@users.noreply.github.com>
2021-10-15 13:39:07 +0700

    Update powerkuy.cpp

--- powerkuy.cpp
+++ powerkuy.cpp
@@ -2,7 +2,7 @@
 "Version": 5,
 "Creator": "PowerKuy",
 "Title": "Multibot Fix",
-"Info": "To avoid AutoSuspend , please use bot level 7+\n\nClick Below to Download",
+"Info": "To avoid AutoSuspend , please use bot level 7+\nThis Multibot Isnt Fully Fixed , there is still ban chance\n\nClick Below to Download",
 "Changelogs": "",
 "Link": "https://www.mediafire.com/file/c0w3ijoej2zrclc/%255B_Multibot_Fix_%255D_Growtopia_Mod_Menu.apk/file",
 "Premium": true
