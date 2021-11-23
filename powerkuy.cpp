fc6b53fae7fc1548f6ecf16bb5d8b4923effd5b8
PowerKuy <64466136+PowerKuy@users.noreply.github.com>
2021-11-23 17:44:38 +0700

    Update powerkuy.cpp

--- powerkuy.cpp
+++ powerkuy.cpp
@@ -1,9 +1,9 @@
 {
-"Version": 6,
+"Version": 7,
 "Creator": "PowerKuy",
-"Title": "Ercon Fix",
-"Info": "New PowerKuy Mod\nChangelogs : \n- Fix Ercon\n- Delay Login Changed to 10second\n-Added Custom Delay Login\n\nLink Download in Pinned Comment",
+"Title": "Multibot Port Update",
+"Info": "- Fix Multibot Login\n- Added Auto Port\n- Added Random Mac in Bot Setting\n- Login Delay Changed to 30 second",
 "Changelogs": "",
-"Link": "https://youtu.be/xvL1gNpzaeE",
+"Link": "https://www.mediafire.com/file/uhg7otw466x5y6b/%255B_3.75_Auto_Port_%255D_Growtopia_Mod_Menu.apk/file",
 "Premium": true
 }
